d6f5f6e8fdcd3ae6d3e4339939dfc4dc7a55a358
orson391 <newtham29@gmail.com>
2025-01-26 12:08:16 +0530

    nugg

--- Major Project/database/house.sql
+++ Major Project/database/house.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 15, 2025 at 03:19 PM
+-- Generation Time: Jan 26, 2025 at 07:37 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,62 @@ SET time_zone = "+00:00";
 --
 -- Database: `house`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `districts`
+--
+
+CREATE TABLE `districts` (
+  `id` int(11) NOT NULL,
+  `dis` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `districts`
+--
+
+INSERT INTO `districts` (`id`, `dis`) VALUES
+(1, 'Alappuzha'),
+(2, 'Ernakulam'),
+(3, 'Idukki'),
+(4, 'Kannur'),
+(5, 'Kasaragod'),
+(6, 'Kollam'),
+(7, 'Kottayam'),
+(8, 'Kozhikode'),
+(9, 'Malappuram'),
+(10, 'Palakkad'),
+(11, 'Pathanamthitta'),
+(12, 'Thiruvananthapuram'),
+(13, 'Thrissur'),
+(14, 'Wayanad');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location`
+--
+
+CREATE TABLE `location` (
+  `id` int(11) NOT NULL,
+  `locations` varchar(30) NOT NULL,
+  `district_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `location`
+--
+
+INSERT INTO `location` (`id`, `locations`, `district_id`) VALUES
+(1, 'Chalakudi', 13),
+(2, 'Mala', 13),
+(3, 'Kodungallur', 13),
+(4, 'Irinjalakuda', 13),
+(5, 'Puthukkad', 13),
+(6, 'Wadakkanchery', 13),
+(7, 'Kunnamkulam', 13);
 
 -- --------------------------------------------------------
 
@@ -46,6 +102,21 @@ INSERT INTO `users` (`id`, `username`, `password`, `user_type`) VALUES
 --
 
 --
+-- Indexes for table `districts`
+--
+ALTER TABLE `districts`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `dis` (`dis`);
+
+--
+-- Indexes for table `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `locations` (`locations`),
+  ADD KEY `fk_location_district` (`district_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -56,10 +127,32 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `districts`
+--
+ALTER TABLE `districts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `location`
+--
+ALTER TABLE `location`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `location`
+--
+ALTER TABLE `location`
+  ADD CONSTRAINT `fk_location_district` FOREIGN KEY (`district_id`) REFERENCES `districts` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
